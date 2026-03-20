91969a0fee931ac04d93be06fcdab86a3fe713c1
enricooxd <henri.mbezerra@gmail.com>
2026-03-20 09:31:41 -0300

    commit tempo att

--- aulas/aula1/calculo_tempo.c
+++ aulas/aula1/calculo_tempo.c
@@ -16,8 +16,25 @@ int main(){
 
     tempo_inicial = clock();
 
-    for(int i=0; i<10; i++){
+    //O(n)
+    for(int i=0; i<1000000; i++){
         soma = soma + 1;
+
+    }
+
+    tempo_final = clock();
+
+    //O(n^2)
+    double duracao = (double)(tempo_final - tempo_inicial) / CLOCKS_PER_SEC;
+    printf("O tempo de execucao foi %.5f segundos\n", duracao);
+
+    for(int i=0; i<1000000; i++){
+
+        for(int i=0; i<1000000; i++){
+
+            soma = soma + 1;
+            
+        }
 
     }
 
